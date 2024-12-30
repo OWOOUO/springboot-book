a1711a0ab67dd5ba25a95c7dadfff3fcdcd7f166
OWOOUO <ggwogns@naver.com>
2024-12-30 19:35:25 +0900

    Initial commit

---
+++ src/test/java/com/kim/book/springboot_book/SpringbootBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.book.springboot_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

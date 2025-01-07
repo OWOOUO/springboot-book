f19c7b09fe8ec2745b8dbf1c100694932fe90ef8
OWOOUO <ggwogns@naver.com>
2025-01-07 14:02:34 +0900

    Add update

---
+++ src/main/java/com/kim/book/springboot_book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.kim.book.springboot_book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
